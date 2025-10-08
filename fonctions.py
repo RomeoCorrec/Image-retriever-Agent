f2ecc565a0412e70fdb172f7ec7db8bcf0460112
Romeo <romeo.correc@gmail.com>
2025-10-08 10:19:23 +0200

    Ajout de 2 nouvelles fonctions permettant d'ajouter des images et des données de visages dans la base de données

--- fonctions.py
+++ fonctions.py
@@ -3,6 +3,9 @@ from PIL import Image
 import torch
 from smolagents import LiteLLMModel
 from huggingface_hub import login
+from deepface import DeepFace
+import uuid
+from qdrant_client.http import models
 
 from pathlib import Path
 # Fonction pour se connecter à Hugging Face
@@ -57,3 +60,120 @@ def load_filled_prompt(template_path: str, QDRANT_URL: str, QDRANT_KEY: str, tas
     """
     template = Path(template_path).read_text(encoding="utf-8")
     return template.format(QDRANT_URL=QDRANT_URL, QDRANT_KEY=QDRANT_KEY, task=task)
+
+
+# Function pour ajouter une image avec les noms des personnes détectées
+def add_image_with_person_name_from_path(image_path, client, add_faces_vector=True, images_collection="images_collection", faces_collection="faces"):
+    embeddings_query = DeepFace.represent(
+        img_path=image_path,
+        model_name="Facenet512",
+        detector_backend="opencv",   # ou mtcnn, mediapipe, opencv
+        align=True,
+        max_faces=10                    # autorise la détection de plusieurs visages
+    )
+
+    names = []
+    scores = []
+    for emb in embeddings_query:
+        embeddings_query_vector = emb['embedding']
+        hits = client.query_points(
+            collection_name=faces_collection,
+            query=embeddings_query_vector,
+            limit=1,
+            with_payload=True,
+        )
+        
+        for point in hits.points:
+            name = point.payload.get("name")
+            names.append(name)
+            scores.append(point.score)
+    
+    # Vérification si l'image est déjà dans la collection d'images
+    existing = client.scroll(
+        collection_name=images_collection,
+        scroll_filter=models.Filter(
+            must=[
+                models.FieldCondition(
+                    key="path",
+                    match=models.MatchValue(value=image_path)
+                )
+            ]
+        ),
+        limit=1
+    )
+    if len(existing[0]) > 0:
+        print(f"Image déjà présente : {image_path}")
+        return names, scores  # on quitte la fonction
+        
+    # Ajout de l'image dans la collection d'images
+    uid = uuid.uuid4()
+    vec = embed_image(image_path)
+
+    client.upsert(
+        collection_name=images_collection,
+        points=[
+            models.PointStruct(
+                id=str(uid),
+                vector=vec.tolist(),
+                payload={
+                    "person": names,
+                    "path": image_path
+                }
+            )
+        ]
+    )
+    return names, scores
+
+# Fonction qui ajoute uniquement le vecteurs d'un seul visage avec le nom fournis, a partir d'une image
+def add_face_with_person_name_from_path(image_path, person_name, client, faces_collection="faces"):
+    embeddings_query = DeepFace.represent(
+        img_path=image_path,
+        model_name="Facenet512",
+        detector_backend="opencv",   # ou mtcnn, mediapipe, opencv
+        align=True,
+    )
+
+    if len(embeddings_query) == 0:
+        print(f"Aucun visage détecté dans l'image : {image_path}")
+        return False
+    elif len(embeddings_query) > 1:
+        print(f"Plusieurs visages détectés dans l'image : {image_path}. Veuillez fournir une image avec un seul visage.")
+        return False
+
+    emb = embeddings_query[0]
+    embeddings_query_vector = emb['embedding']
+    
+    # # Vérification si le visage est déjà dans la collection de visages
+    # existing = client.scroll(
+    #     collection_name=faces_collection,
+    #     scroll_filter=models.Filter(
+    #         must=[
+    #             models.FieldCondition(
+    #                 key="name",
+    #                 match=models.MatchValue(value=person_name)
+    #             )
+    #         ]
+    #     ),
+    #     limit=1
+    # )
+    # if len(existing[0]) > 0:
+    #     print(f"Visage déjà présent pour la personne : {person_name}")
+    #     return False  # on quitte la fonction
+        
+    # Ajout du visage dans la collection de visages
+    uid = uuid.uuid4()
+
+    client.upsert(
+        collection_name=faces_collection,
+        points=[
+            models.PointStruct(
+                id=str(uid),
+                vector=embeddings_query_vector,
+                payload={
+                    "name": person_name,
+                    "image_path": image_path
+                }
+            )
+        ]
+    )
+    return True
